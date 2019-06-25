f838401a8fec7409e63b5b89fde749fcbfdeb663
Pony <pony@ponycool.com>
2019-06-25 14:34:02 +0800

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,15 +1,10 @@
 <?php namespace App\Controllers;
-
-use CodeIgniter\Controller;
 use App\Models\UserModel;
-
 class Home extends BaseController
 {
-	public function index()
-	{
-		return view('welcome_message');
-	}
-
-	//--------------------------------------------------------------------
-
+    public function index()
+    {
+        return view('welcome_message');
+    }
+    //--------------------------------------------------------------------
 }
