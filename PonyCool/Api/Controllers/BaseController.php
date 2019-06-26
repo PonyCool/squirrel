4924871a29a917b30e05270ad9a7b4dbe7911e5a
Pony <pony@ponycool.com>
2019-06-26 15:43:08 +0800

    登陆认证
    登陆认证

---
+++ PonyCool/Api/Controllers/BaseController.php
@@ -0,0 +1,78 @@
+<?php
+declare(strict_types=1);
+
+namespace PonyCool\Api\Controllers;
+
+use CodeIgniter\Controller;
+use CodeIgniter\API\ResponseTrait;
+use CodeIgniter\HTTP\{RequestInterface, ResponseInterface};
+use Psr\Log\LoggerInterface;
+use PonyCool\Libraries\Code;
+use OutOfRangeException;
+
+class BaseController extends Controller
+{
+    use ResponseTrait;
+
+    /**
+     * An array of helpers to be loaded automatically upon
+     * class instantiation. These helpers will be available
+     * to all other controllers that extend BaseController.
+     *
+     * @var array
+     */
+    protected $helpers = [];
+
+    /**
+     * Constructor.
+     *
+     */
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        // Do Not Edit This Line
+        parent::initController($request, $response, $logger);
+        //--------------------------------------------------------------------
+        // Preload any models, libraries, etc, here.
+        //--------------------------------------------------------------------
+        // E.g.:
+        // $this->session = \Config\Services::session();
+
+    }
+
+    /**
+     * 渲染数据
+     * @param null $data
+     * @return ResponseInterface
+     */
+    protected function render($data = null): ResponseInterface
+    {
+        $resp = [
+            'status' => 'success',
+            'code' => 0,
+            'message' => '',
+            'data' => []
+        ];
+        if (is_string($data)) {
+            $resp['message'] = $data;
+        }
+        if (is_array($data)) {
+            $code = $data['code'] ?? Code::FAIL();
+            try {
+                if (!is_object($code)) {
+                    throw new OutOfRangeException();
+                }
+                $resp['code'] = $code->getValue();
+            } catch (OutOfRangeException $exception) {
+                $resp['code'] = Code::FAIL()->getValue();
+            }
+            $resp['status'] = $resp['code'] ? 'unsuccess' : 'success';
+            $resp['message'] = $data['message'] ?? '';
+            unset($data['code']);
+            unset($data['message']);
+            if (!empty($data) && 0 === $resp['code']) {
+                $resp['data'] = $data;
+            }
+        }
+        return $this->respond($resp, 200);
+    }
+}
--- PonyCool/Libraries/Code.php
+++ PonyCool/Libraries/Code.php
@@ -7,11 +7,32 @@ use MyCLabs\Enum\Enum;
 
 class Code extends Enum
 {
-    private const TEST = '1';
-    private const VIEW = 'view';
-    private const EDIT = 'edit';
-    const DEMO='1';
-//    public static function VIEW() {
-//        return new Code(self::VIEW);
-//    }
+    // 成功
+    private const SUCCESS = 0;
+    // 通用错误
+    private const FAIL = 1;
+    // 资源未找到
+    private const NOT_FOUND = 2;
+    // 未授权
+    private const UNAUTHORIZED = 3;
+
+    public static function SUCCESS()
+    {
+        return new Code(self::SUCCESS);
+    }
+
+    public static function FAIL()
+    {
+        return new Code(self::FAIL);
+    }
+
+    public static function NOT_FOUND()
+    {
+        return new Code(self::NOT_FOUND);
+    }
+
+    public static function UNAUTHORIZED()
+    {
+        return new Code(self::UNAUTHORIZED);
+    }
 }
